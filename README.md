18fdc544b6127d465b26805b2a9795c004b47075
Lukasz Lucko <lukaszl95@o2.pl>
2018-01-24 11:52:44 +0100

    zadanie-13.3

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # zadanie-13.3
+https://lukaszl95.github.io/zadanie-13.3/
---
+++ program.js
@@ -0,0 +1,18 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+        	case 'exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+            case '':
+            process.stderr.write('Wrong instruction!\n');
+            console.log(process.env);
+            break;
+        } 
+    }
+});
